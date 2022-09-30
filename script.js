bfcaa29d9211ceea6a33df453af272e2f019937e
Miki Fukazawa <>
2022-09-30 20:07:06 +0900

    no message

--- script.js
+++ script.js
@@ -45,18 +45,17 @@ $(function () {
     $('#box2').css('color','#d4b16e');
 });
 
-// // sbtitle
-// const CLASSNAME = "-visible";
-// const TIMEOUT = 2000;
-// const $target = $(".bg");
-
-// setInterval(() => {
-//   $target.addClass(CLASSNAME);
-
-//   setTimeout(() => {
-//     $target.removeClass(CLASSNAME);
-//   }, TIMEOUT);
-// }, TIMEOUT*2);
+// マーカー
+$(window).scroll(function (){
+    $(".js-markerScrollAnimation").each(function(){
+      var position = $(this).offset().top; //ページの一番上から要素までの距離を取得
+      var scroll = $(window).scrollTop(); //スクロールの位置を取得
+      var windowHeight = $(window).height(); //ウインドウの高さを取得
+      if (scroll > position - windowHeight){ //スクロール位置が要素の位置を過ぎたとき
+        $(this).addClass('is-active'); //クラス「active」を与える
+      }
+    });
+});
 
 // foam buttom
 $(function(){
